6ebe3b840cc5cbc70b8bd394a8cd56b3e0923138
nathan3boy <nathan3boss@gmail.com>
2023-01-04 03:39:21 -0300

    feat(app): added custom cursor

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from 'next/app';
+import dynamic from 'next/dynamic';
 import { ThemeProvider } from 'styled-components';
 
 import Menu from '../components/menu';
@@ -7,6 +8,10 @@ import theme from '../global/theme';
 import 'remixicon/fonts/remixicon.css';
 import GlobalStyle from '../global';
 import { LayoutApp } from '../styles/app.styles';
+
+const AnimatedCursor = dynamic(() => import('react-animated-cursor'), {
+  ssr: true,
+}) as any;
 
 const App = ({ Component, pageProps }: AppProps) => (
   <ThemeProvider theme={theme}>
@@ -17,6 +22,28 @@ const App = ({ Component, pageProps }: AppProps) => (
 
       <Component {...pageProps} />
     </LayoutApp>
+
+    <AnimatedCursor
+      innerSize={20}
+      outerSize={30}
+      color="91, 209, 156"
+      outerAlpha={0.2}
+      innerScale={0.7}
+      hasBlendMode
+      outerScale={1.2}
+      clickables={[
+        'a',
+        'input[type="text"]',
+        'input[type="email"]',
+        'input[type="number"]',
+        'input[type="submit"]',
+        'input[type="image"]',
+        'label[for]',
+        'select',
+        'textarea',
+        'button',
+      ]}
+    />
   </ThemeProvider>
 );
 
